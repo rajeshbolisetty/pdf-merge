c7eb3a0998b448ea6403b44dbf225d2f9ebf5668
rajesh.bolisetty <rajesh.bnv@gmail.com>
2023-12-15 21:01:22 +1300

    added github action for deploy

--- src/helpers/merge-and-download.tsx
+++ src/helpers/merge-and-download.tsx
@@ -1,16 +1,16 @@
 import PDFMerger from 'pdf-merger-js/browser';
 
 export async function mergeAndDownloadPDFs(files: File[]) {
-    const merger = new PDFMerger();
-    for (const file of files) {
-        await merger.add(file);
-    }
-    const mergedPdf = await merger.saveAsBlob();
-    const url = URL.createObjectURL(mergedPdf);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'test.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const merger = new PDFMerger();
+  for (const file of files) {
+    await merger.add(file);
+  }
+  const mergedPdf = await merger.saveAsBlob();
+  const url = URL.createObjectURL(mergedPdf);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'merged.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
