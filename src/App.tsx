25a971e29d051e0050d7c77920d7a88103815c4d
rajesh.bolisetty <rajesh.bnv@gmail.com>
2023-12-15 21:07:12 +1300

    remove unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import './App.css';
 import FileUpload from './components/fileUpload/fileupload';
-import MButton from './components/button/button'
 
 function App() {
   return (
@@ -9,7 +7,11 @@ function App() {
       <h1>Combine multiple PDFs into one</h1>
       <p>Merge PDFs online without downloading additional software.</p>
       <div className="border rounded ms-5 me-5">
-        <FileUpload text="upload files" className="mt-5 mb-5" fileFormats="application/pdf" />
+        <FileUpload
+          text="upload files"
+          className="mt-5 mb-5"
+          fileFormats="application/pdf"
+        />
       </div>
     </div>
   );
